ece407fe0e4ad48f0735e5a227ff51e8e260ccac
Phonekham <phone.madridista@gmail.com>
2019-05-15 14:45:21 +0700

    Basic Features Syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Phone', age: 24 },
+      { name: 'test', age: 25 }
+    ]
+  }
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 24 },
+        { name: 'testclick', age: 25 }
+      ]
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "newName", age: 24 },
+        { name: event.target.value, age: 25 }
+      ]
+    })
+  }
+  render() {
+    const styles = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: "pointer"
+    };
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <button style={styles} onClick={() => this.switchNameHandler("phonkham")}>Switch the Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Phonekham2')} changed={this.nameChangedHandler}>Hobbies: coding</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
